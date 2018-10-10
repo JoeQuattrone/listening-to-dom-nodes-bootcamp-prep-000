4fd814441b153188d96e6883f9a2c899b25c8e80
Joe Quattrone <joequattrone5@gmail.com>
2018-10-10 14:34:08 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -11,3 +11,14 @@ const input = document.querySelector('input');
 input.addEventListener('keydown', function(e) {
   console.log(e.which);
 });
+
+
+const input = document.querySelector('input')
+ 
+input.addEventListener('keydown', function(e) {
+  if (e.which === 71) {
+    return e.preventDefault()
+  } else {
+    console.log(e.which)
+  }
+});
